3e8fcb3ea7b23988c39effd498227a4652f872cb
Vladimir Papazov <papazovvladimir88@gmail.com>
2023-02-16 22:50:18 +0300

    dsadas

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from flasgger import Swagger, swag_from
+
+swagger_config = {
+    "headers": [],
+    "specs": [
+        {
+            "endpoint": 'api_docs',
+            "route": '/api_docs',
+        }
+    ],
+    "specs_route": "/api/",
+    "static_url_path": "/flasgger_static",
+    "swagger_ui": True
+}
 
 app = Flask(__name__)
+swagger = Swagger(app, swagger_config)
 
-@app.route("/")
+@app.route("/auth/v.1.0", methods=["POST"])
+@swag_from("Auth.yaml", methods=["POST"])
 def index():
-    return "Hello"
+    params = request.get_json()
+    return jsonify({"phone": params["phone"], "password": params["password"]})
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
