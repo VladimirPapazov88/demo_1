a798d905d7a149962877ceeac300a8c051994050
VladimirPapazov88 <papazovvladimir88@gmail.com>
2023-02-17 10:25:28 +0300

    dsadasdas

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
 from flask import Flask, jsonify, request
 from flasgger import Swagger, swag_from
+import pymysql, datetime
+import hashlib
+import os
+
+host = ""
+port = ""
+try:
+    host = os.environ["HOST"]
+    port = os.environ["PORT"]
+except:
+    host = "localhost"
+    port = "8006"
+
+mysqlpass = ""
+with open('secrets/mysql_pass.txt', 'r') as f:
+    mysqlpass = f.read()
+
+print(mysqlpass)
+
+db = pymysql.connect(host=host, port=int(port), user='root', password=mysqlpass, database='db')
+
+cursor = db.cursor()
 
 swagger_config = {
     "headers": [],
@@ -21,6 +43,11 @@ swagger = Swagger(app, swagger_config)
 @swag_from("Auth.yaml", methods=["POST"])
 def index():
     params = request.get_json()
+    phone = params["phone"]
+    passhash = hashlib.md5(params["password"].encode("utf-8")).hexdigest()
+    sql = "INSERT INTO `users` (`date_insert`, `date_update`, `phone`, `password_hash`) VALUES (%s, %s, %s, %s)"
+    cursor.execute(sql, (str(datetime.datetime.now()), str(datetime.datetime.now()), phone, passhash))
+    db.commit()
     return jsonify({"phone": params["phone"], "password": params["password"]})
 
 
--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-pip3 install -r requirements.txt &> /dev/null && python3 main.py
+pip3 install -r requirements.txt &> /dev/null && HOST=mysql-py_api PORT=3306 python3 main.py
