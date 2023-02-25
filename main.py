db04502e58d05f9278608f18885cc1fe1ba52ba6
Vladimir Papazov <papazovvladimir88@gmail.com>
2023-02-25 21:08:24 +0300

    phone check

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify, request
 from flasgger import Swagger, swag_from
-import pymysql, datetime
+from dateutil.relativedelta import relativedelta
+import pymysql
+import datetime
 import hashlib
 import os
 
@@ -17,9 +19,8 @@ mysqlpass = ""
 with open('secrets/mysql_pass.txt', 'r') as f:
     mysqlpass = f.read()
 
-print(mysqlpass)
-
-db = pymysql.connect(host=host, port=int(port), user='root', password=mysqlpass, database='db')
+db = pymysql.connect(host=host, port=int(port), user='root',
+                     password=mysqlpass, database='db')
 
 cursor = db.cursor()
 
@@ -39,16 +40,46 @@ swagger_config = {
 app = Flask(__name__)
 swagger = Swagger(app, swagger_config)
 
+
 @app.route("/auth/v.1.0", methods=["POST"])
 @swag_from("Auth.yaml", methods=["POST"])
 def index():
     params = request.get_json()
     phone = params["phone"]
     passhash = hashlib.md5(params["password"].encode("utf-8")).hexdigest()
-    sql = "INSERT INTO `users` (`date_insert`, `date_update`, `phone`, `password_hash`) VALUES (%s, %s, %s, %s)"
-    cursor.execute(sql, (str(datetime.datetime.now()), str(datetime.datetime.now()), phone, passhash))
-    db.commit()
-    return jsonify({"phone": params["phone"], "password": params["password"]})
+
+    allphonesSQL = "SELECT EXISTS(SELECT `phone` FROM `users` WHERE phone=%s)"
+    cursor.execute(allphonesSQL, (phone,))
+    exist = cursor.fetchone()
+
+    print(exist)
+
+    error = "Номер телефона некорректен"
+    try:
+        if (phone[0:3] == "+7(") and (phone[6] == ")") and (phone[7:10].isdigit()) and (phone[10] == "-") and (phone[11:13].isdigit()) and (phone[13] == "-") and (phone[14:16].isdigit()) and (len(phone) == 16):
+            if (exist[0] == 1):
+                error = "Такой аккаунт уже есть"
+            else:
+                error = ""
+    except IndexError:
+        pass
+
+    if (error == ""):
+        sql = "INSERT INTO `users` (`date_insert`, `date_update`, `phone`, `password_hash`) VALUES (%s, %s, %s, %s)"
+        sql2 = "INSERT INTO `users_tokens` (`date_insert`, `date_update`, `user_id`, `token`, `expires`) VALUES (%s, %s, (SELECT `id` FROM `users` WHERE (`password_hash` = %s)), UUID(), %s)"
+        cursor.execute(sql, (str(datetime.datetime.now()), str(
+            datetime.datetime.now()), phone, passhash))
+        cursor.execute(sql2, (str(datetime.datetime.now()), str(datetime.datetime.now(
+        )), passhash, str(datetime.datetime.now()+relativedelta(months=2))))
+        db.commit()
+        sql3 = "SELECT `token` FROM `users_tokens` WHERE (`user_id` = (SELECT `id` FROM `users` WHERE (`password_hash` = %s)))"
+        cursor.execute(sql3, (passhash,))
+        token = cursor.fetchone()
+        sql4 = "SELECT `expires` FROM `users_tokens` WHERE (`user_id` = (SELECT `id` FROM `users` WHERE (`password_hash` = %s)))"
+        cursor.execute(sql4, (passhash,))
+        expires = cursor.fetchone()
+        return jsonify({"success": True, "error": "", "data": {"token": token[0], "expires": expires[0]}})
+    return jsonify({"success": False, "error": error, "data": {}})
 
 
 if __name__ == "__main__":
